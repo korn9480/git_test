d1f4c776de6ea85da37c8fffde6eae5a4aa3db0c
tinnakorn <tub7005@gmail.com>
2021-11-19 20:44:00 +0700

    add user input and read file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import codecs
 print("tinkon")
-name=input()
-print("your name :",name)
+name=input("enter your name :")
+age = int(input("enter your age :"))
+print("%s age %d".format(name,age))
+
+with codecs.open("./test.txt","r",encoding="utf-8")as f :
+    text = f.read()
+print(text)
